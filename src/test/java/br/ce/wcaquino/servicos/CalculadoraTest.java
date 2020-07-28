c33d3026330fc299e3fceb2f2ea8e483a30fc98f
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-07-28 18:59:15 -0300

    TDD

---
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraTest.java
@@ -0,0 +1,55 @@
+package br.ce.wcaquino.servicos;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculadoraTest {
+	
+	public Calculadora calc;
+	
+	@Before
+	public void setup() {
+		calc = new Calculadora();
+	}
+
+	@Test
+	public void deveSomarDoisNumeros() {
+		//cenario
+		int a = 5;
+		int b = 3;
+		
+		//acao
+		int resultado = calc.soma(a, b);
+		
+		//verificacao
+		Assert.assertEquals(8, resultado);
+	}
+	
+	@Test
+	public void deveSubstrairDoisNumeros() {
+		//cenario
+		int a = 5;
+		int b = 3;
+		
+		//acao
+		int resultado = calc.substrair(a, b);
+		
+		//verificacao
+		Assert.assertEquals(2, resultado);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
