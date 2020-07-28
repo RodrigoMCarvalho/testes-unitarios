b1dfd665de866c8b14e1b65025099c0fe33c059d
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-07-28 18:46:20 -0300

    tratando exceções

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -2,43 +2,26 @@ package br.ce.wcaquino.servicos;
 
 import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
-import br.ce.wcaquino.exceptions.LocadoraException;
 
 public class LocacaoService {
 	
-	public Locacao alugarFilme(Usuario usuario, List<Filme> filmes) throws LocadoraException, FilmeSemEstoqueException {
+	public Locacao alugarFilme(Usuario usuario, Filme filme) throws Exception {
 		
-		if(filmes == null) {
-			throw new LocadoraException("Filme vazio");
-		}
-		
-		for (Filme filme: filmes) {
-			if(filme.getEstoque() == 0) {
-				throw new FilmeSemEstoqueException();
-			}
-		}
-		
-		if(usuario == null) {
-			throw new LocadoraException("Usuário vazio");
+		if(filme.getEstoque() == 0) {
+			throw new Exception("Filme sem estoque");
 		}
 		
 		Locacao locacao = new Locacao();
-		locacao.setFilmes(new ArrayList<Filme>(filmes));
+		locacao.setFilme(filme);
 		locacao.setUsuario(usuario);
 		locacao.setDataLocacao(new Date());
-		Double valorTotal = 0d;
-		for (Filme filme: filmes) {
-			valorTotal += filme.getPrecoLocacao();
-		}
-		locacao.setValor(valorTotal);
+		locacao.setValor(filme.getPrecoLocacao());
+
 		//Entrega no dia seguinte
 		Date dataEntrega = new Date();
 		dataEntrega = adicionarDias(dataEntrega, 1);
