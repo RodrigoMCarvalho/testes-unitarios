c0ba06a63bbdc8c866966edd80bae496edd8a2c8
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-07-28 18:43:01 -0300

    TDD

---
+++ src/test/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -0,0 +1,13 @@
+package br.ce.wcaquino.servicos;
+
+public class Calculadora {
+
+	public int soma(int a, int b) {
+		return a + b;
+	}
+
+	public int substrair(int a, int b) {
+		return a - b;
+	}
+
+}
