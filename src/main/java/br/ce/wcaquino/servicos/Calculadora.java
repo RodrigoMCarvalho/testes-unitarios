de3b06fdd765c291a62fdddadeac9a1344ddef5e
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-08-04 16:12:05 -0300

    spy

--- src/main/java/br/ce/wcaquino/servicos/Calculadora.java
+++ src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -3,11 +3,16 @@ package br.ce.wcaquino.servicos;
 public class Calculadora {
 
 	public int soma(int a, int b) {
+		System.out.println("Método soma");
 		return a + b;
 	}
 
 	public int substrair(int a, int b) {
 		return a - b;
+	}
+	
+	public void imprime() {
+		System.out.println("Passei aqui");
 	}
 
 }
--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -1,14 +1,50 @@
 package br.ce.wcaquino.servicos;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 
 public class CalculadoraMockTest {
+	
+	@Mock
+	private Calculadora calcMock;
+	
+	@Spy
+	private Calculadora calcSpy;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void devoMostrarDoferencaMockSpy() {
+		when(calcMock.soma(1, 5)).thenReturn(8);
+		//when(calcSpy.soma(1, 5)).thenReturn(8);
+		doReturn(5).when(calcSpy).soma(1, 2);
+		doNothing().when(calcSpy).imprime();
+		
+		
+		System.out.println("Mock:" + calcMock.soma(1, 2));
+		System.out.println("Spy:" + calcSpy.soma(1, 2));
+		
+		System.out.println("Mock");
+		calcMock.imprime();      //padrão do mock é não executar
+		System.out.println("Spy");
+		calcSpy.imprime();			//padrão do spy é executar, porém devido ao doNothing não foi executado
+	}
+	
+	
 	
 	@Test
 	public void teste() {
@@ -19,7 +55,6 @@ public class CalculadoraMockTest {
 		
 		assertEquals(5, calc.soma(1, 10000));	
 		System.out.println(argCapt.getAllValues());
-		
 	}
 
 }
